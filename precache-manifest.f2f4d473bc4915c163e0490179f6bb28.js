18a9b2f83fac0ae147d59124a9c0dc9ecf9269ba
Bojana Karakacev <bojana.karakacev@gmail.com>
2020-01-18 11:59:22 +0100

    Updates

---
+++ precache-manifest.f2f4d473bc4915c163e0490179f6bb28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37346f869579d6553a17d924cbee0dec",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "76726445c31b1a061d28",
+    "url": "/redux-todo/static/css/main.542abee2.chunk.css"
+  },
+  {
+    "revision": "a3c55ad66b2b7583563d",
+    "url": "/redux-todo/static/js/2.48ee2386.chunk.js"
+  },
+  {
+    "revision": "76726445c31b1a061d28",
+    "url": "/redux-todo/static/js/main.d1914a7a.chunk.js"
+  },
+  {
+    "revision": "4d319af47e499f6431d1",
+    "url": "/redux-todo/static/js/runtime-main.31d2c4e4.js"
+  }
+]);
